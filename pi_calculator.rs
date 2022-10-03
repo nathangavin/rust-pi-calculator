8efd68b5ac45c8372faedc0dd4244e436d11c7b7
Nathan <nathangavin987@gmail.com>
2022-10-03 20:54:02 +1300

    piCalculator Psudocode

---
+++ pi_calculator.rs
@@ -0,0 +1,44 @@
+/*
+    max_value = 1000000
+
+    primes = new list
+
+    primes[] = 2
+    print prime
+    primes[] = 3
+    print prime
+
+    first candidate = 5
+
+    while candidate < max_value
+        if candidate isPrime
+            primes[] = candidate
+            print prime
+        endif
+        candidate += 2
+        if candidate isPrime
+            primes[] = candidate
+            print prime
+        endif
+        candidate += 4
+    endwhile
+
+
+
+    function isPrime - candidate
+        isPrime = true
+        sqrt = square root of candidate
+
+        for all primes
+            val = prime
+            if val > sqrt
+                break
+            else if $candidate % val == 0 AND candidate != val
+                isPrime = false
+                break
+            endif
+        endfor
+
+        return isPrime
+    endfunc
+*/
